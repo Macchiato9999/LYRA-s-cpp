0e2af5011f5349f62a8a75726b4877b380916856
Lyra <3098595797@qq.com>
2025-04-05 17:09:39 +0800

    Add files via upload

---
+++ 前缀和.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	int a[1000];
+    int s[1000];
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+		s[i] = a[i]+s[i-1];
+	}
+	while (m--)
+	{
+		int l, r;
+		cin >> l >> r;
+		cout << s[r] - s[l - 1] << endl;
+	}
+	return 0;
+}
+
+
+
